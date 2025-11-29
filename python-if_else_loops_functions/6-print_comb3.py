ea011fc4a5e82a4b63df4f665489f4a4319cb5fe
root <12702@holbertonstudents.com>
2025-11-29 09:52:39 +0000

    all different combination of 2-digit numbers

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(i, 10):
         if j != i:
             if i == 8 and j == 9:
-                print('{}{}'.format(i,j), end='\n')
+                print('{}{}'.format(i, j), end='\n')
                 break
-            print('{}{}'.format(i,j), end=', ')
+            print('{}{}'.format(i, j), end=', ')
