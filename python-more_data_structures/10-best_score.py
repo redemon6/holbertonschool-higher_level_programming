3e71673d904b16f120823014cd865b9ce733dd0b
root <root@60af361c708a4a388e31a4649f0d52c3-2377118072>
2025-12-04 11:32:47 +0000

    k

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for i in a_dictionary.keys():
             if a_dictionary[i] == max(a_dictionary.values()):
                 return i
