a317e4917dad447bb52c3f7873ba2e020877afa7
root <root@f273cb28ae084405a45b36c39a8b7f31-2377118072>
2025-12-13 13:24:44 +0000

    dgdg

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Defines an empty class, BaseGeometry, intended as a base for geometric shapes.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class serving as the base for geometry-related classes.
+    """
+    pass
