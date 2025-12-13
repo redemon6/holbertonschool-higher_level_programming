272f21c1234171d3e97f874e4a4b86d04fca2986
root <root@f273cb28ae084405a45b36c39a8b7f31-2377118072>
2025-12-13 05:31:44 +0000

    k

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Module 0-rectangle
+Defines an empty class Rectangle.
+This module serves as the foundation for defining a Rectangle object.
+"""
+
+
 class Rectangle:
+    """
+    The Rectangle class defines a rectangle object.
+
+    Currently, this class is empty and serves as a placeholder for
+    future attributes and methods.
+    """
     pass
