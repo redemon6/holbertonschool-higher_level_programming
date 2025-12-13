988d37c0d7452d917928edca2b2f5bc8627fa16d
root <root@f273cb28ae084405a45b36c39a8b7f31-2377118072>
2025-12-13 13:43:09 +0000

    kggu

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+dyttynyjkxdkkkhj
+'''
+class BaseGeometry:
+    '''
+    htdrhrhtjjy'''
+    def area(self):
+        '''exception
+        '''
+        raise Exception("area() is not implemented")
