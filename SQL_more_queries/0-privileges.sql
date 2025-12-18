1e2dd97a577d2de21270f4cf5d6cedd57851d4c1
root <root@d848173088b64e868c46373e48d3609c-2377118072>
2025-12-18 11:27:08 +0000

    gdf

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 --to show all granted privileges to user
-SHOW GRANTS FOR USER IF EXISTS 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
