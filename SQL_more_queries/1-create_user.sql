c19c960002185bb26f046a42792065e12150b07e
root <root@ade2cfa4136a488b86f29a5bb223228a-2377118072>
2025-12-19 16:13:40 +0000

    fgbgf

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create user with password and give all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES TO 'user_0d_1'@'localhost';
